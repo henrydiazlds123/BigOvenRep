26dc2429af36efcc8302ab1ac313a7fc18eea119
Gabi Staheli <sta11017@byui.edu>
2014-01-15 18:15:00 +0000

    Adding first version

---
+++ demo.js
@@ -0,0 +1,54 @@
+function changeColor()
+{
+   alert(" Now you will regret cliking me!!");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+   alert(" :P");
+
+   var titleSpan = document.getElementById("pageTitle");
+   titleSpan.style.backgroundColor = "purple";
+}
