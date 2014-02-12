632f6cd6b5c4b4050138c035c0304a90aa96b8da
twilightPony123 <sta11017@byui.edu>
2014-02-12 06:29:53 +0000

    TicTacToe Added

---
+++ ticTacToe/Player.java
@@ -0,0 +1,15 @@
+public class Player{
+   char letter;
+   char getLetter()
+   {
+     return letter;
+   }
+   void setLetter(char newLetter)
+   {
+      letter = newLetter;
+   }
+   public static void main (String[] args)
+   {
+      System.out.println("compiles :) ");
+   }
+}
