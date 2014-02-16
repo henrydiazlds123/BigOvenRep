53d498e803f84c91c0be47cd2e44454f59b7904b
twilightPony123 <sta11017@byui.edu>
2014-02-16 04:56:46 +0000

    Tried to add a textfield, but wouldn't show

--- expo4.java
+++ expo4.java
@@ -7,7 +7,7 @@ import java.applet.*;
 import java.awt.*;
 import javax.swing.*;
 
-public class expo4
+public class expo4 extends Applet
 implements ActionListener
 {
    //got help for textfield from http://www.willamette.edu/~gorr/classes/cs231/lectures/chapter5/
@@ -16,42 +16,39 @@ implements ActionListener
    TextField inputLine = new TextField(40);
    
    public expo4() {
-     // add(inputLine);
+      add(inputLine);
+         inputLine.addActionListener(this);
+
    }
    
-   private static void createAndShowGUI()
-   {
-      JFrame.setDefaultLookAndFeelDecorated(true);
-      
-      JFrame frame = new JFrame("");
-      
-      JButton addToStartList = new JButton("Name");
-      addToStartList.addActionListener(new expo4());
-      
-      frame.getContentPane().add(addToStartList);
-      
-      //     String inputLine;
-      
-      
-      
-      //Display window
-      frame.pack();
-      frame.setVisible(true);
-      
-      
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   }
+   private static void createAndShowGUI() {
+   JFrame.setDefaultLookAndFeelDecorated(true);
+
+   JFrame frame = new JFrame("");
+  // add(inputLine);
+   JButton addToStartList = new JButton("Name");
+   addToStartList.addActionListener(new expo4());
    
+   frame.getContentPane().add(addToStartList);
+   frame.getContentPane().add(addToStartList);
+  // frame.getContentPane().add(inputLine);
+ //  String inputLine;
    
-   public static void main(String[] args)
-   {
-      new expo4().run();
-   }
-   
-   public void run() {
-      createAndShowGUI();
-   }
-           
+
+ 
+   //Display window
+   frame.pack();
+   frame.setVisible(true);
+   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+public static void main(String[] args) {
+new expo4().run();
+}
+public void run() {
+createAndShowGUI();
+}
+        
+       
     public void actionPerformed(ActionEvent event)
    {
       String direction = inputLine.getText();
