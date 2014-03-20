a59c603605b6b1f75e155925e806aea30f8d0297
twilightPony123 <sta11017@byui.edu>
2014-03-20 00:49:50 +0000

    Sorry

--- examples/ta2.js
+++
@@ -1,5 +0,0 @@
-function checkEmail(email)
-{
-alert("Parsing the text in: " + email.value);
-alert("Checking your email");
-}
--- library/logout.php
+++
@@ -1,8 +0,0 @@
-<?php session_start();
-
-      unset($_SESSION[user]);
-      unset($_SESSION['fn']);
-      unset($_SESSION[priv]);
-
-      header('Location: index.php');
-?>
